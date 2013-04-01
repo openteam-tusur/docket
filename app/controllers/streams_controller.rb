eb323714eef7c757a7f0b0f131a63d23f60ff259
Manin Iliy <bkmzvf@gmail.com>
2013-04-01 21:49:01 +0700

    make up done

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -5,4 +5,16 @@ class StreamsController < InheritedResources::Base
   has_scope :sorted_by_code, :default => 1 do |controller, scope|
     scope.order(:code)
   end
+
+  def create
+    create! { plan_path(@plan) }
+  end
+
+  def update
+    update! { plan_path(@plan) }
+  end
+
+  def destroy
+    destroy! { plan_path(@plan) }
+  end
 end
